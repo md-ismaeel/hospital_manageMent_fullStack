31966f60bfdd544c37ca6cc4307ae0de5986b1da
Md Ismail <mdismaeelkhan345@gmail.com>
2024-07-31 17:00:10 +0530

    admin updated

--- Frontend/src/Utils/utils.js
+++ Frontend/src/Utils/utils.js
@@ -1,6 +1,6 @@
 // Backend end pints
-
 // export const API_USER_BACKEND = "http://localhost:10000/api/v1/user";
+
 export const API_USER_BACKEND = "https://hospital-management-8e00.onrender.com/api/v1/user";
 
 export const requestOptions = {
